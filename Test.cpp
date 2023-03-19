bb98077cea6d9ef3897b1a3d4a89a25f58db6a3d
AndrewTate <luqmanh@student.usm.my>
2023-03-19 16:30:55 +0800

    Add CPP Code

---
+++ Test.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+void CalcAvg(int [3]);
+
+int main(){
+    
+  const int size=3;
+  int array[size];
+
+  cout << "Enter the values: " << endl;
+  
+  for (int i=0; i<size; i++){
+    cin >> array[i];
+  }
+
+  CalcAvg(array);
+
+  return 0;
+
+}
+
+void CalcAvg(int arr[3]) {
+
+  double sum=0, avg;
+
+
+  for (int i=0; i<3; i++){
+    sum+=arr[i];
+  }
+
+  avg = sum/3;
+
+  cout << "The average value is: " << avg << endl;
+
+}
+
+
